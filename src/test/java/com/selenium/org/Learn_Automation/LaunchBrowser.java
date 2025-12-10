6c3dafe056460e7a4bea088adce4a8a4fb6f61d5
Arati0218 <aratiagrawal0218@gmail.com>
2025-12-10 10:09:16 -0700

    Headless Browser

--- src/test/java/com/selenium/org/Learn_Automation/LaunchBrowser.java
+++ src/test/java/com/selenium/org/Learn_Automation/LaunchBrowser.java
@@ -2,12 +2,16 @@ package com.selenium.org.Learn_Automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class LaunchBrowser {
 	WebDriver driver;
 	
 	public WebDriver startBrowser() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+
         driver = new ChromeDriver();
         ConfigReader config = new ConfigReader();
         driver.get(config.getAppURL());
